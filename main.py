d2f44f2bec1b4af3e4f39785bf98911b9a74653e
Jakns99 <j.schermerhorn99@gmail.com>
2025-03-19 18:47:00 -0700

    Added starter JSON Functionality.

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import os
 import json
 
+# JSON Functionality
+#----------------------------------------------------------------------------------------------------------------------#
+TASK_FILE = "tasks.json"
 
+def savefile(tasks):
+    """Save tasks to a JSON file."""
+    try:
+        with open(TASK_FILE, "w") as file:
+            json.dump(tasks, file)
+        print("Tasks saved successfully.")
+    except Exception as e:
+        print(f"Error saving tasks file: {e}")
 
-
+def loadfile():
+    """Load tasks from a JSON file."""
+    if os.path.exists(TASK_FILE):
+        try:
+            with open(TASK_FILE, "r") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print("Error, file not found.")
+            return []
+#----------------------------------------------------------------------------------------------------------------------#
 
 
 
