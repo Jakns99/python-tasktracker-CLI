7299bdab7171b9f32c5d08105bff66363e78dd45
Jakns99 <j.schermerhorn99@gmail.com>
2025-03-19 18:35:39 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+
+
+
+
+
+
+
+
+
+
+
+# TODO 1. Add, Update, and Delete Tasks Functions
+# TODO 2. Mark a task as in progress or done Function
+# TODO 3. List all tasks Function
+# TODO 4. List all tasks that are not done Function
+# TODO 5. List all tasks that are in progress Function
